2a4f08b8f6eb6943f194b544e5232af149818f3f
Allen <490683824@qq.com>
2015-08-22 07:23:28 -0700

    modified main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,68 @@
 #include <muduo/net/TcpServer.h>
 #include <muduo/base/AsyncLogging.h>
 #include <muduo/base/Logging.h>
+#include <muduo/net/EventLoop.h>
+#include <muduo/net/InetAddress.h>
 
 #include <string>
+#include <map>
+
+using namespace muduo;
+using namespace muduo::net;
+
+typedef std::map<string,string> UserMap;
+UserMap users;
+
+/*
+string getUser(const string &user)
+{
+    string result = "No such user";
+    UserMap::iterator iter = users.find(user);
+    if ( iter != users.end() ) {
+        result = iter->second;
+    }
+    return result;
+}
+
+
+
+void onMessage(const TcpConnectionPtr &conn,
+                Buffer *buf,
+                Timestamp receiveTime)
+{
+    const char *crlf = buf->findCRLF();
+    if (crlf) {
+        string user(buf->peek(),crlf);
+        conn->send(getUser(user) + "\r\n");
+        buf->retrieveUntil(crlf + 2);
+        conn->shutdown();
+    }
+}
+*/
+
+/*
+std::pair<std::string,int> getWordCount(MyDictionary *ptr_dic,string seach_word) {
+    std::pair<std::string,int> result = ( ptr_dic->find(seach_word))->second;
+    return result;
+}
+*/
+
+
+int getDistance(string a,string b);
+
+void onMessage(const TcpConnectionPtr &conn,
+                Buffer *buf,
+                Timestamp receiveTime)
+{
+    const char *crlf = buf->findCRLF();
+    if (crlf) {
+        string user(buf->peek(),crlf);
+        conn->send(getUser(user) + "\r\n");
+        buf->retrieveUntil(crlf + 2);
+        conn->shutdown();
+    }
+}
+
 
 int main(void) {
     
@@ -35,6 +95,18 @@ int main(void) {
     //获取字典
     std::string dict_path = my_conf_map["my_dict_path"];
     MyDictionary *pMydict = MyDictionary::createInstance(dict_path.c_str());
+
+
+    //测试网络库
+    
+    users["allen"] = "handsome and clever";
+
+    EventLoop loop;
+
+    TcpServer server(&loop,InetAddress(1079),"Finger");
+    server.setMessageCallback(onMessage);
+    server.start();
+    loop.loop();
 
     
     
